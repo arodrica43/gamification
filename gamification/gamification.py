a6ace35753710902a3596ec9c23a1aa38581284a
Alexº <lexws33@gmail.com>
2020-11-18 00:20:40 +0100

    automatic update for image build

--- gamification/gamification.py
+++ gamification/gamification.py
@@ -68,8 +68,8 @@ class GamificationXBlock(StudioEditableXBlockMixin, XBlock):
         """
         # Just to show data coming in...
         #assert data['hello'] == 'world'
-        xblock_id = self.scope_ids.def_id
-        return {"xblock_id": xblock_id}
+        #xblock_id = self.scope_ids.def_id
+        return {"xblock_id": 0}
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
