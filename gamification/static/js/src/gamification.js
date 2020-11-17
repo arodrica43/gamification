46ce115475b523752840b000507abfb704b79a01
Alexº <lexws33@gmail.com>
2020-11-17 23:22:18 +0100

    solving bugs on getting XBlock Ids

--- gamification/static/js/src/gamification.js
+++ gamification/static/js/src/gamification.js
@@ -1,36 +1,36 @@
 /* Javascript for GamificationXBlock. */
 function GamificationXBlock(runtime, element) {
 
-    // function updateCount(result) {
-    //     $('.count', element).text(result.count);
-    // }
+    function fillXBlock(result) {
 
-    // var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+      fetch("https://agmodule.herokuapp.com/api/g_mechanics/1/?user=user1")
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(myJson){
+       
+      console.log('#incr-' + result.xblock_id);
+      $('#incr-' + result.xblock_id, element).innerHTML = (myJson.html);
+       //document.querySelector("#incr-" + result.xblock_id).innerHTML = (myJson.html);
+       $(myJson.html).appendTo(document.body);
+      })
+      .catch(function(error){
+       console.log("Error: " + error);
+      });
 
-    // $('p', element).click(function(eventObject) {
-    //     $.ajax({
-    //         type: "POST",
-    //         url: handlerUrl,
-    //         data: JSON.stringify({ "hello": "world" }),
-    //         success: updateCount
-    //     });
-    // });
+    }
+
+    var handlerUrl = runtime.handlerUrl(element, 'get_xblock_data');
+
 
     $(function($) {
         /* Here's where you'd do things on page load. */
-     fetch("https://agmodule.herokuapp.com/api/g_mechanics/1/?user=user1")
-      .then(function(response){
-	alert(1);
-	return response.json();
-      })
-      .then(function(myJson){
-      	console.log("{self.scope_ids.def_id}");
-	document.querySelector("#incr-{self.scope_ids.def_id}").innerHTML = (myJson.html);
-	$(myJson.html).appendTo(document.body);
-      })
-      .catch(function(error){
-	console.log("Error: " + error);
-      });
+        $.ajax({
+            type: "POST",
+            url: handlerUrl,
+            data: JSON.stringify({ "hello": "world" }),
+            success: fillXBlock
+        });
 
     });
 }
