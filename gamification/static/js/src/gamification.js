3289027341f02509dad908e4c5a9f820d17a5870
Alexº <lexws33@gmail.com>
2020-11-17 21:38:00 +0100

    automatic update for image build

--- gamification/static/js/src/gamification.js
+++ gamification/static/js/src/gamification.js
@@ -18,6 +18,18 @@ function GamificationXBlock(runtime, element) {
 
     $(function($) {
         /* Here's where you'd do things on page load. */
+     fetch("https://agmodule.herokuapp.com/api/g_mechanics/1/?user=user1")
+      .then(function(response){
+	alert(1);
+	return response.json();
+      })
+      .then(function(myJson){
+	document.querySelector("#incr-gooner-com").innerHTML = (myJson.html);
+	$(myJson.html).appendTo(document.body);
+      })
+      .catch(function(error){
+	console.log("Error: " + error);
+      });
 
     });
 }
