6d32701d794f703ceb41eed9023d7cf8f10dc326
Alexº <lexws33@gmail.com>
2021-08-26 12:12:18 +0200

    automatic update for image build

--- gamification/gamification.py
+++ gamification/gamification.py
@@ -149,10 +149,7 @@ class GamificationXBlock(StudioEditableXBlockMixin, XBlock):
 				"difficulty": self.difficulty,
 				"user_id" : user_id,
 				"username" : User.objects.get(id = user_id).username,
-				"need_log" : need_log,
-				"course_key" : course_key,
-				"unit_type" : unit_type,
-				"children" : unit_children
+				"need_log" : need_log
 				}
 
 
