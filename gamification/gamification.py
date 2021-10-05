ed9c3ca8b34732e9f48426b62c8dd235fc4c5d65
Alexº <lexws33@gmail.com>
2021-10-05 17:11:13 +0200

    automatic update for image build

--- gamification/gamification.py
+++ gamification/gamification.py
@@ -196,7 +196,7 @@ class GamificationXBlock(StudioEditableXBlockMixin, XBlock):
 			"course_id" : str(course_id),
 			"tab_id" : tab_id,
 			"unit_id" : unit_id,
-			"pipe" : str(leafs[index - 1].has_submitted_answer())
+			"pipe" : "nothing"
 			}
 
 	@XBlock.json_handler
